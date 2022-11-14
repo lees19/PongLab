8a2d5a142ab8a07bf0e612a5b8f5616efb92241d
Sunny Lee <lees19@wit.edu>
2022-11-14 11:48:36 -0500

    update venv

--- pong/Scripts/activate.bat
+++ pong/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\lees19\Desktop\homework\DataScience\pong
+set VIRTUAL_ENV=C:\Users\lees19\Documents\GitHub\PongLab\pong
 
 if not defined PROMPT set PROMPT=$P$G
 
